5067de48b4809ca38f3512708245bb6cacdbfb3c
yagmurkocaoglu <yagmurkocaogluu@gmail.com>
2019-09-19 17:13:10 +0300

    NotAl
    C# ve MSSQL

---
+++ NotAl/NotAl/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace NotAl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNot = new System.Windows.Forms.RichTextBox();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncel = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.güncelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.silToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNot
+            // 
+            this.txtNot.Location = new System.Drawing.Point(12, 34);
+            this.txtNot.Name = "txtNot";
+            this.txtNot.Size = new System.Drawing.Size(155, 96);
+            this.txtNot.TabIndex = 0;
+            this.txtNot.Text = "";
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(67, 150);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncel
+            // 
+            this.btnGuncel.Location = new System.Drawing.Point(67, 180);
+            this.btnGuncel.Name = "btnGuncel";
+            this.btnGuncel.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncel.TabIndex = 3;
+            this.btnGuncel.Text = "Güncelle";
+            this.btnGuncel.UseVisualStyleBackColor = true;
+            this.btnGuncel.Click += new System.EventHandler(this.btnGuncel_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.güncelleToolStripMenuItem,
+            this.silToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(121, 48);
+            // 
+            // güncelleToolStripMenuItem
+            // 
+            this.güncelleToolStripMenuItem.Name = "güncelleToolStripMenuItem";
+            this.güncelleToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.güncelleToolStripMenuItem.Text = "Güncelle";
+            this.güncelleToolStripMenuItem.Click += new System.EventHandler(this.güncelleToolStripMenuItem_Click);
+            // 
+            // silToolStripMenuItem
+            // 
+            this.silToolStripMenuItem.Name = "silToolStripMenuItem";
+            this.silToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.silToolStripMenuItem.Text = "Sil";
+            this.silToolStripMenuItem.Click += new System.EventHandler(this.silToolStripMenuItem_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(197, 34);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(220, 169);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Notlar";
+            this.columnHeader1.Width = 213;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 243);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnGuncel);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.txtNot);
+            this.Name = "Form1";
+            this.Text = "Notlarım";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtNot;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem güncelleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem silToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ NotAl/NotAl/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NotAl.Model;
+
+ namespace NotAl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listele();
+            btnGuncel.Enabled = false;
+        }
+        LeCahierEntities2 db = new LeCahierEntities2();
+         void Listele()
+        {
+            listView1.Items.Clear();
+            foreach (Note not in db.Notes.ToList())
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = not.Note1;
+                lvi.Tag = not.ID;
+                listView1.Items.Add(lvi);
+            }
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            Note n = new Note();
+            n.Note1 = txtNot.Text;
+
+            db.Notes.Add(n);
+            bool result = db.SaveChanges() > 0;
+            Listele();
+            MessageBox.Show(result ? "OK":"Sıkıntı Var");
+            txtNot.Text = " ";
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            db.Notes.Remove(db.Notes.Find((int)listView1.SelectedItems[0].Tag));
+            bool result = db.SaveChanges() > 0;
+            Listele();
+            MessageBox.Show(result ? "OK":"Sıkıntı Var");
+
+        }
+        Note updtd;
+        private void güncelleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            updtd = db.Notes.Find((int)listView1.SelectedItems[0].Tag);
+            txtNot.Text = updtd.Note1;
+            btnGuncel.Enabled = true;
+        }
+
+        private void btnGuncel_Click(object sender, EventArgs e)
+        {
+                updtd.Note1 = txtNot.Text;
+                bool result = db.SaveChanges() > 0;
+                Listele();
+                MessageBox.Show(result ? "OK" : "Sıkıntı Var");
+                txtNot.Text = " ";
+            
+        }
+    }
+}
